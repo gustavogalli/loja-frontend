186b9744875ed0b5d6e87da9f9f4ed914dfcbd25
gustavogalli <gustavosgalli@gmail.com>
2024-08-23 10:30:34 -0300

    Creating Login component

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  form: any = {};
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    this.authService.login(this.form).subscribe(
+      resp => {
+        alert('Login realizado com sucesso!');
+        localStorage.setItem('user', JSON.stringify(resp));
+        this.router.navigate(['/home']);
+      },
+      err => console.error('Credenciais inválidas.')
+    );
+  }
+}
