8c25cd0346031fb6d3abc5aa8227d1f7d1931a69
gustavogalli <gustavosgalli@gmail.com>
2024-08-23 10:30:48 -0300

    Creating Cadastrar component

---
+++ src/app/components/auth/cadastrar/cadastrar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-cadastrar',
+  templateUrl: './cadastrar.component.html',
+  styleUrls: ['./cadastrar.component.css']
+})
+export class CadastrarComponent {
+  form: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.register(this.form).subscribe(
+      data => console.log('Usuário cadastrado com sucesso.'),
+      err => console.error('Erro ao cadastrar usuário.')
+    );
+  }
+}
